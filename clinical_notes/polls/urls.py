7251c49eb8deb4cf33f447c94e4b51b8a0a1f1e0
Rafael Oleques Nunes <rafael.oleques@acad.pucrs.br>
2018-04-19 17:00:51 -0300

    Criado o dicionario com as palavras em ingles, tela em ingles em desenvolvimentos, palavras em portugues foram adicionadas na evolucao de ID 12, resolvidos erros relacionados ao usuario por um id, na url, que seja maior ou menor aos disponiveis

--- clinical_notes/polls/urls.py
+++ clinical_notes/polls/urls.py
@@ -10,8 +10,9 @@ from django.conf.urls import url
 ###
 
 from . import views
-from polls.views import PacientePageView
+from polls.views import PacientePageView, PacientePageViewEng
 
 urlpatterns = [
-	path('', PacientePageView.as_view(), name='index'),
+	#path('', PacientePageView.as_view(), name='index'),
+	path('', PacientePageView.as_view(), name='index')
 ]
--- clinical_notes/polls/views.py
+++ clinical_notes/polls/views.py
@@ -27,13 +27,14 @@ class PacientePageView(TemplateView):
 		return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
 	def get_context_data(self, **kwargs):
-
-		indice = 12
-
 		context = super(PacientePageView, self).get_context_data(**kwargs)
 
 		prescription = pd.read_csv('./excel_evol.csv.gz', compression='gzip', nrows=50000)
-		wordModel = KeyedVectors.load_word2vec_format('health_w2v_unigram_50.bin', binary=True)
+
+		indice = int(self.request.GET.get('id'))
+
+		if  (0 > indice) or (indice >= ( len(prescription.loc[::]) ) ) or indice == None:
+			indice = 0
 
 		m = prescription.loc[indice] #valor que ha' aqui
 
@@ -52,12 +53,6 @@ class PacientePageView(TemplateView):
 		cont = 0
 		for palavra in evolucao:
 
-			## Cria lista de palavras similares
-			sem_assento = self.remove_accents(palavra)
-			palavra_similar = []
-			if palavra in wordModel.vocab:
-				palavra_similar = wordModel.most_similar_cosmul(sem_assento,topn=20)
-
 			evolucao[cont] = palavra
 
 			## Busca palavra no Mesh
@@ -74,14 +69,14 @@ class PacientePageView(TemplateView):
 						if(d['qualifier'] == 'anatomy & histology'):
 							start_underline = '<span class = "anatomy">'
 							end_underline = '</span>'
-						elif(d['qualifier'] == 'pharmacology'):
-							start_underline = '<span class = "medication">'
-							end_underline = '</span>'
 						elif(d['qualifier'] == 'methods'):
 							start_underline = '<span class = "procedure">'
 							end_underline = "</span>"
 						elif(d['qualifier'] == 'diagnosis'):
 							start_underline = '<span class = "diagnosis">'
+							end_underline = '</span>'
+						elif(d['qualifier'] == 'pharmacology'):
+							start_underline = '<span class = "medication">'
 							end_underline = '</span>'
 						else:
 							start_underline = ''
@@ -104,5 +99,89 @@ class PacientePageView(TemplateView):
 		context['data'] = m['DATA EVOL']
 		context['registro'] = m['REG. PACIENTE']
 		context['evolucao'] = strr
+		context['indice_avancar'] = int(indice+1)
+		context['indice_retornar'] = indice-1
+		context['ultima_posicao'] = len(m) - 1
 		return context
 
+###################################################
+
+
+class PacientePageViewEng(TemplateView):
+	template_name = 'index.html'
+	
+	def remove_accents(self, input_str):
+		nfkd_form = unicodedata.normalize('NFKD', input_str)
+		return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
+
+	def get_context_data(self, **kwargs):
+		context = super(PacientePageViewEng, self).get_context_data(**kwargs)
+
+		prescription = pd.read_csv('./excel_evol_eng.csv.gz', compression='gzip', nrows=50000)
+
+		indice = int(self.request.GET.get('id'))
+
+		if  (0 > indice) or (indice >= ( len(prescription.loc[::]) ) ) or indice == None:
+			indice = 0
+		m = prescription.loc[indice]
+
+		evolucao = m['DADOS DA EVOLUÇÃO'].split(' ');
+		#Passando pelo xml
+		
+		with gzip.open('dictMesh.dict_eng.gz','rb') as fp:
+			dictMesh = pickle.load(fp)
+			fp.close()
+
+		valida = False
+
+		#Verifica a lista para ver se a palavra esta no dicionario
+		cont = 0
+		for palavra in evolucao:
+
+			evolucao[cont] = palavra
+
+			## Busca palavra no Mesh
+			for dui in dictMesh:
+				d = dictMesh[dui]
+				for t in d['terms']:
+					new_t = t.replace('<i>', '')
+					new_t = new_t.replace('</i>', '')
+					if new_t.lower() == palavra.lower():
+						teste = dictMesh[dui]['terms']
+						termos = '<br/>- '.join(teste)
+						
+
+						if(d['qualifier'] == 'anatomy & histology'):
+							start_underline = '<span class = "anatomy">'
+							end_underline = '</span>'
+						elif(d['qualifier'] == 'methods'):
+							start_underline = '<span class = "procedure">'
+							end_underline = "</span>"
+						elif(d['qualifier'] == 'diagnosis'):
+							start_underline = '<span class = "diagnosis">'
+							end_underline = '</span>'
+						elif(d['qualifier'] == 'pharmacology'):
+							start_underline = '<span class = "medication">'
+							end_underline = '</span>'
+						else:
+							start_underline = ''
+							end_underline = ''
+
+						d['name'] = d['name'].replace('[', ' [')
+						evolucao[cont] = start_underline+'<a style="color:inherit; text-decoration:none" href="#" data-id="<strong>ID: </strong>'+d['ID']+'<br/><br/>" data-name="<h3><a target= \'_blank\' href=\'https://meshb.nlm.nih.gov/record/ui?ui='+dui+'\'>'+d['name']+'<a></h3><br/>"  data-terms="<strong>Termos semelhantes:</strong><br/>- '+termos+'" data-scope="<strong>Definicao:</strong> '+d['scope']+'">'+palavra+'</a>'+end_underline
+						break
+
+					
+
+			
+			cont +=1
+
+		strr = ' '.join(evolucao)
+		
+		context['data'] = m['DATA EVOL']
+		context['registro'] = m['REG. PACIENTE']
+		context['evolucao'] = strr
+		context['indice_avancar'] = indice+1
+		context['indice_retornar'] = indice-1
+		context['ultima_posicao'] = len(m) - 1
+		return context
