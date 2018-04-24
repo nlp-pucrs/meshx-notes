80ab1dc7540753ad270af031fbcb3b86fa82d956
Rafael Oleques Nunes <rafael.oleques@acad.pucrs.br>
2018-04-24 18:07:54 -0300

    Melhorando a busca, criacao do indice reverso, iniciacao da pesquisa com o ultimo.

--- clinical_notes/polls/views.py
+++ clinical_notes/polls/views.py
@@ -11,6 +11,7 @@ from django.views.generic.base import TemplateView
 from django.template import loader
 
 import pandas as pd
+import os
 
 import gzip, pickle
 import string
@@ -21,7 +22,7 @@ import unicodedata
 
 class PacientePageView(TemplateView):
 	template_name = 'index.html'
-		
+
 	def remove_accents(self, input_str):
 		nfkd_form = unicodedata.normalize('NFKD', input_str)
 		return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
@@ -39,16 +40,23 @@ class PacientePageView(TemplateView):
 			lingua = 'pt'
 
 		if(lingua == 'pt'):
-			caminho_evolucao = './excel_evol.csv.gz'
-			caminho_dicionario = 'dictMesh.dict.gz'
+			caminho_evolucao = '../excel_evol.csv.gz'
+			caminho_dicionario = '../dictMesh.dict.gz'
+			caminho_indice = '../indiceReversoPT.dict.gz'
 			definicao = 'Definicao'
 			termos_semelhantes = 'Termos semelhantes'
 		elif(lingua == 'en'):
-			caminho_evolucao = './excel_evol_eng.csv.gz'
-			caminho_dicionario = 'dictMesh.dict_eng.gz'
+			caminho_evolucao = '../excel_evol_eng.csv.gz'
+			caminho_dicionario = '../dictMesh.dict_eng.gz'
+			caminho_indice = '../indiceReversoEN.dict.gz'
 			definicao = 'Definition'
 			termos_semelhantes = 'Similar terms'
 
+
+		current_path = os.path.dirname(os.path.realpath(__file__))
+		caminho_evolucao = os.path.join(current_path, caminho_evolucao)
+		caminho_dicionario = os.path.join(current_path, caminho_dicionario)
+		caminho_indice = os.path.join(current_path, caminho_indice)
 
 		prescription = pd.read_csv(caminho_evolucao, compression='gzip', nrows=50000)
 
@@ -70,73 +78,87 @@ class PacientePageView(TemplateView):
 		evolucao = m['DADOS DA EVOLUÇÃO'].split(' ');
 
 		#Abrindo o dicionario
-		
-		with gzip.open(caminho_dicionario,'rb') as fp:
-			dictMesh = pickle.load(fp)
-			fp.close()
+
+		with gzip.open(caminho_indice,'rb') as fd:
+			indiceReverso = pickle.load(fd)
+			fd.close()
+
+		with gzip.open(caminho_dicionario,'rb') as fd:
+			dictMesh = pickle.load(fd)
+			fd.close()
 
 		#Verifica a lista para ver se a palavra esta no dicionario
 		valida = 0
 		for i in range(len(evolucao)):
 
-			palavra = evolucao[i]
+			palavra = evolucao[i].lower()
+			palavra_tri = palavra.lower()+" "+evolucao[i+1].lower()+" "+evolucao[i+2].lower()
+			palavra_bi = palavra.lower()+" "+evolucao[i+1].lower()
+			palavra_i = '<i>' + palavra + '</i>'
+			ID = ''
 
-			## Busca palavra no Mesh
-			for dui in dictMesh:
-				d = dictMesh[dui]
-				for t in d['terms']:
-					#Retira as tags de italico na hora de comparar com as palavras da evolucao
-					new_t = t.replace('<i>', '')
-					new_t = new_t.replace('</i>', '')	
+			### pesquisa trigramas
+			if palavra_tri in indiceReverso:
+				ID = indiceReverso[palavra_tri]
+			### pesquisa bigrama
+			
 
-					valida = 0
+			### pesquisa unigrama
+			elif palavra in indiceReverso:
+				ID = indiceReverso[palavra]
+			elif palavra_i in indiceReverso:
+				ID = indiceReverso[palavra_i]
 
-					#Verifica se e um bigram, se for, vai apagar o proximo para nao aparecer repetido
-					if (i+2 < (len(evolucao)) and palavra.lower()+" "+evolucao[i+1].lower()+" "+evolucao[i+2].lower() == new_t.lower()):
-						valida = 3	
-						evolucao[i+1] = ''
-						evolucao[i+2] = ''
-					elif (i+1 < (len(evolucao)) and palavra.lower()+" "+evolucao[i+1].lower() == new_t.lower()):
-						valida = 2	
-						evolucao[i+1] = '' 
-					elif new_t.lower() == palavra.lower():#Verifica se e um unigram
-						valida = 1
+			if ID != '':
+			#for indice_termos in  indiceReverso:
 
-					if valida != 0:
-						teste = dictMesh[dui]['terms']
-						termos = '<br/>- '.join(teste)
-						
-						#Verifica o qualifier, se ha, entao salva, senao bota vazio
+				new_t = indice_termos.replace('<i>', '')
+				new_t = new_t.replace('</i>', '')
 
-						if(d['qualifier'] == 'anatomy & histology'):
-							start_underline = '<span class = "anatomy">'
-							end_underline = '</span>'
-						elif(d['qualifier'] == 'methods'):
-							start_underline = '<span class = "procedure">'
-							end_underline = "</span>"
-						elif(d['qualifier'] == 'diagnosis'):
-							start_underline = '<span class = "diagnosis">'
-							end_underline = '</span>'
-						elif(d['qualifier'] == 'pharmacology'):
-							start_underline = '<span class = "medication">'
-							end_underline = '</span>'
-						elif(d['qualifier'] == '#'):
-							start_underline = '<span class = "other">'
-							end_underline = '</span>'
-						else:
-							start_underline = ''
-							end_underline = ''
+				valida = 0
 
-						#Sobrescreve a que tinha e bota com uma nova com o link etc.
+				#Verifica se e um bigram, se for, vai apagar o proximo para nao aparecer repetido
+				if (i+2 < (len(evolucao)) and palavra.lower()+" "+evolucao[i+1].lower()+" "+evolucao[i+2].lower() == new_t.lower()):
+					valida = 3	
+					evolucao[i+1] = ''
+					evolucao[i+2] = ''
+				elif (i+1 < (len(evolucao)) and palavra.lower()+" "+evolucao[i+1].lower() == new_t.lower()):
+					valida = 2	
+					evolucao[i+1] = '' 
+				elif new_t.lower() == palavra.lower():#Verifica se e um unigram
+					valida = 1
 
-						d['name'] = d['name'].replace('[', ' [')
-						evolucao[i] = start_underline+'<a style="color:inherit; text-decoration:none" href="#" data-id="<strong>ID: </strong>'+d['ID']+'<br/><br/>" data-name="<h3><a target= \'_blank\' href=\'https://meshb.nlm.nih.gov/record/ui?ui='+dui+'\'>'+d['name']+'<a></h3><br/>"  data-terms="<strong>'+termos_semelhantes+':</strong><br/>- '+termos+'" data-scope="<strong>'+definicao+':</strong> '+d['scope']+'">'+new_t+'</a>'+end_underline
-						
-						break
-				#Se ele ja achou a palavra, entao sai do loop e vai para a proxima
 				if valida != 0:
-					break
+					ID = indiceReverso[indice_termos]['ID']	
+					teste = dictMesh[ID]['terms']
+					termos = '<br/>- '.join(teste)
+					
+					#Verifica o qualifier, se ha, entao salva, senao bota vazio
 
+					if(dictMesh[ID]['qualifier'] == 'anatomy & histology'):
+						start_underline = '<span class = "anatomy">'
+						end_underline = '</span>'
+					elif(dictMesh[ID]['qualifier'] == 'methods'):
+						start_underline = '<span class = "procedure">'
+						end_underline = "</span>"
+					elif(dictMesh[ID]['qualifier'] == 'diagnosis'):
+						start_underline = '<span class = "diagnosis">'
+						end_underline = '</span>'
+					elif(dictMesh[ID]['qualifier'] == 'pharmacology'):
+						start_underline = '<span class = "medication">'
+						end_underline = '</span>'
+					elif(dictMesh[ID]['qualifier'] == '#'):
+						start_underline = '<span class = "other">'
+						end_underline = '</span>'
+					else:
+						start_underline = ''
+						end_underline = ''
+
+					#Sobrescreve a que tinha e bota com uma nova com o link etc.
+
+					nome = dictMesh[ID]['name'].replace('[', ' [')
+					evolucao[i] = start_underline+'<a style="color:inherit; text-decoration:none" href="#" data-id="<strong>ID: </strong>'+ID+'<br/><br/>" data-name="<h3><a target= \'_blank\' href=\'https://meshb.nlm.nih.gov/record/ui?ui='+ID+'\'>'+dictMesh[ID]['name']+'<a></h3><br/>"  data-terms="<strong>'+termos_semelhantes+':</strong><br/>- '+termos+'" data-scope="<strong>'+definicao+':</strong> '+dictMesh[ID]['scope']+'">'+new_t+'</a>'+end_underline
+					
 		#Junta tudo novamente
 
 		strr = ' '.join(evolucao)
